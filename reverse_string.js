86f5d83ca420b076fdae70106a8780b871d8a209
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-01 09:37:42 -0500

    Update reverse_string.js

--- reverse_string.js
+++ reverse_string.js
@@ -1,3 +1,7 @@
+/*Reverse the provided string and return the reversed string.
+
+For example, "hello" should become "olleh". */
+
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
